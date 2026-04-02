547e17ca4f18a437ea3d036c15012e386fba6ece
Bernhard Nebel <nebel@hinterm-ziel.de>
2026-04-02 23:57:10 +0200

    T861, T43U, T26 passed SPI master test, T26 needed an additional bit in boards.txt

--- avr/extras/tests/spi/mspi/mspi.ino
+++ avr/extras/tests/spi/mspi/mspi.ino
@@ -1,7 +1,8 @@
 // Master SPI sketch: Send 'hello slave' and receive 'hi master'
 #include <SPI.h>
 
-#define TXBIT 4
+//#define TXBIT 4
+#define CORR_OSCCAL 0x68 // for 43U
 
 const int len = 12;
 char send[12] = "hello slave";
@@ -10,9 +11,18 @@ char recv[12] = "hi master";
 char buf[12];
 
 void setup (void) {
-   //Serial.setTxBit(4);
+#if RAMEND > 2047
+#ifdef CORR_OSCCAL
+   OSCCAL = CORR_OSCCAL;
+#endif
+#ifdef TXBIT
+   Serial.setTxBit(TXBIT);
+#endif
    Serial.begin(9600); //set baud
    Serial.println(F("\n\r\n\rSPI Master test sketch"));
+   Serial.flush();
+#endif
+   //pinMode(SS, OUTPUT);
    digitalWrite(SS, HIGH); // disable Slave Select
    SPI.begin ();
    SPI.setClockDivider(SPI_CLOCK_DIV8);//divide the clock by 8
@@ -23,19 +33,34 @@ void loop (void) {
    char c;
    digitalWrite(SS, LOW); // enable Slave Select
    // send test string
+#if RAMEND > 2027
    Serial.println(F("Sending: 'hello slave'"));
+#endif
    for (i=0; i < len; i++) {
       c = SPI.transfer (send[i]);
       delayMicroseconds(10);
       if (i > 0) buf[i-1] = c; 
    }
+#if RAMEND > 2047
    Serial.println();
    Serial.print(F("Received: "));
    Serial.println(buf);
+#endif
    digitalWrite(SS, HIGH); // disable Slave Select
+#if RAMEND > 2047
    if (strcmp(recv, buf) == 0)
      Serial.println(F("SPI test successful"));
    else
      Serial.println(F("SPI test failed"));
+#else 
+   if (strcmp(recv, buf) == 0)
+      success();
+#endif
    while (1);
 }
+
+void success()
+{
+   while (2);
+}
+
